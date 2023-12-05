d9aa4ac3b9aacd3470db2b0e545dbae7912219e5
Lucas Nascimento <la7371180@gmail.com>
2023-12-05 16:06:05 -0300

    Alterando o nome do arquivo

--- Dia 03/index.js
+++ Dia 03/index.js
@@ -7,5 +7,5 @@ let peso = Number(prompt("Digite o seu peso"));
 let anoDeNascimento = (2023 - idade);
 let imc = peso / (altura * altura);
 
-// demonstração no console
+// demonstração no consolee
 console.log("Olá Lucas, você tem " + idade + " anos, nasceu em " + anoDeNascimento + ", tem " + altura + " de altura, pesa " + peso + "kg e seu imc é de " + imc + "kg/m2");
